c19782c29f032aaa5e49738bdd4df623ff4553a6
Steven Haryanto (on PC) <stevenharyanto@gmail.com>
2011-08-20 17:53:34 +0700

    add files, move gen_usage() from Sub::Spec::CmdLine

---
+++ lib/Sub/Spec/To/Text/Usage.pm
@@ -0,0 +1,144 @@
+package Sub::Spec::To::Text::Usage;
+
+use 5.010;
+use strict;
+use warnings;
+
+use Sub::Spec::Utils; # tmp, for _parse_schema
+
+sub _parse_schema {
+    Sub::Spec::Utils::_parse_schema(@_);
+}
+
+sub gen_usage($;$) {
+    require Data::Dump::Partial;
+    require List::MoreUtils;
+
+    my ($sub_spec, $opts) = @_;
+    $opts //= {};
+
+    my $usage = "";
+
+    my $cmd = $opts->{cmd};
+    if ($sub_spec->{name}) {
+        $cmd = ($sub_spec->{_package} ? "$sub_spec->{_package}::" : "") .
+            $sub_spec->{name};
+    }
+    if ($sub_spec->{summary}) {
+        $usage .= ($cmd ? "$cmd - " : "") . "$sub_spec->{summary}\n\n";
+    }
+
+    my $desc = $sub_spec->{description};
+    if ($desc) {
+        $desc =~ s/^\n+//; $desc =~ s/\n+$//;
+        $usage .= "$desc\n\n";
+    }
+
+    my $args  = $sub_spec->{args} // {};
+    my $rargs = $sub_spec->{required_args};
+    $args = { map {$_ => _parse_schema($args->{$_})} keys %$args };
+    my $has_cat = grep { $_->{attr_hashes}[0]{arg_category} }
+        values %$args;
+    my $prev_cat;
+    my $noted_star_req;
+    for my $name (sort {
+        (($args->{$a}{attr_hashes}[0]{arg_category} // "") cmp
+             ($args->{$b}{attr_hashes}[0]{arg_category} // "")) ||
+                 (($args->{$a}{attr_hashes}[0]{arg_pos} // 9999) <=>
+                      ($args->{$b}{attr_hashes}[0]{arg_pos} // 9999)) ||
+                          ($a cmp $b) } keys %$args) {
+        my $arg = $args->{$name};
+        my $ah0 = $arg->{attr_hashes}[0];
+
+        my $cat = $ah0->{arg_category} // "";
+        if (!defined($prev_cat) || $prev_cat ne $cat) {
+            $usage .= "\n" if defined($prev_cat);
+            $usage .= ($cat ? ucfirst("$cat options") :
+                           ($has_cat ? "General options" :
+                                ($opts->{options_name} ?
+                                     "$opts->{options_name} options" :
+                                         "Options")));
+            $usage .= " (* denotes required options)"
+                unless $noted_star_req++;
+            $usage .= ":\n";
+            $prev_cat = $cat;
+        }
+
+        my $arg_desc = "";
+
+        if ($arg->{type} eq 'any') {
+            my @schemas = map {_parse_schema($_)} @{$ah0->{of}};
+            my @types   = map {$_->{type}} @schemas;
+            @types      = sort List::MoreUtils::uniq(@types);
+            $arg_desc  .= "[" . join("|", @types) . "]";
+        } else {
+            $arg_desc  .= "[" . $arg->{type} . "]";
+        }
+
+        my $o = $ah0->{arg_pos};
+        my $g = $ah0->{arg_greedy};
+
+        $arg_desc .= " $ah0->{summary}" if $ah0->{summary};
+        $arg_desc .= " (one of: ".
+            Data::Dump::Partial::dumpp($ah0->{in}).")"
+                  if defined($ah0->{in});
+        $arg_desc .= " (default: ".
+            Data::Dump::Partial::dumpp($ah0->{default}).")"
+                  if defined($ah0->{default});
+
+        my $aliases = $ah0->{arg_aliases};
+        if ($aliases) {
+            $arg_desc .= "\n";
+            for (sort keys %$aliases) {
+                my $alinfo = $aliases->{$_};
+                $arg_desc .= join(
+                    "",
+                    "      ",
+                    (length == 1 ? "-$_" : "--$_"), " ",
+                    $alinfo->{summary} ? $alinfo->{summary} :
+                        "is alias for '$name'",
+                    "\n"
+                );
+            }
+        }
+
+        my $desc = $ah0->{description};
+        if ($desc) {
+            $desc =~ s/^\n+//; $desc =~ s/\n+$//;
+            # XXX format/rewrap
+            $desc =~ s/^/      /mg;
+            $arg_desc .= "\n$desc\n";
+        }
+
+        $usage .= sprintf("  --%-25s %s\n",
+                          $name . ($ah0->{required} ? "*" : "") .
+                              (defined($o) ? " [or arg ".($o+1).
+                                  ($g ? "-last":"")."]" : ""),
+                          $arg_desc);
+    }
+
+    if ($sub_spec->{cmdline_examples}) {
+        $usage .= "\nExamples:\n\n";
+        my $cmd = $opts->{cmd} // $0;
+        for my $ex (@{ $sub_spec->{cmdline_examples} }) {
+            $usage .= " % $cmd $ex->{cmd}\n";
+            my $desc = $ex->{description};
+            if ($desc) {
+                $desc =~ s/^\n+//; $desc =~ s/\n+$//;
+                $usage .= "\n$desc\n\n";
+            }
+        }
+    }
+
+    $usage;
+}
+
+1;
+# ABSTRACT: Generate usage/help message from sub spec
+__END__
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=cut
