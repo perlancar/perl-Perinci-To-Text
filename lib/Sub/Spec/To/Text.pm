9f289c32856f56a8e5aea9807a65953f1d88987a
Steven Haryanto (on PC) <stevenharyanto@gmail.com>
2011-08-30 10:42:23 +0700

    add placeholder module: Sub::Spec::To::Text

---
+++ lib/Sub/Spec/To/Text.pm
@@ -0,0 +1,23 @@
+package Sub::Spec::To::Text;
+
+# VERSION
+
+1;
+# ABSTRACT: Generate text documentation from spec
+
+=head1 SYNOPSIS
+
+ # use Sub::Spec::To::Text qw(spec_to_text);
+ # my $text = spec_to_text(spec=>$spec, ...);
+
+
+=head1 DESCRIPTION
+
+EMPTY MODULE AT THE MOMENT.
+
+
+=head1 SEE ALSO
+
+L<Sub::Spec::To::Text::Usage>
+
+=cut
