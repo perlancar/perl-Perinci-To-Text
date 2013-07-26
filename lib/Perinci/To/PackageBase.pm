6164f45409a90bee9ce484e3e6f68f4e2d4ae999
Steven Haryanto (on PC) <stevenharyanto@gmail.com>
2013-07-26 19:07:53 +0700

    WIP Update to new role interface 0.53+

--- lib/Perinci/To/PackageBase.pm
+++ lib/Perinci/To/PackageBase.pm
@@ -12,8 +12,6 @@ with 'SHARYANTO::Role::I18N';
 with 'SHARYANTO::Role::I18NRinci';
 
 has url => (is=>'rw');
-has function_sections => (is => 'rw');
-#has method_sections => (is => 'rw');
 has _pa => (
     is => 'rw',
     lazy => 1,
@@ -46,292 +44,14 @@ sub BUILD {
         'functions',
         'links',
     ];
-    $self->{function_sections} //= [
-        'summary',
-        'description',
-        'arguments',
-        'result',
-        'examples',
-        'links',
-    ];
-    #$self->{method_sections} //= $self->{function_sections};
 }
 
-sub add_doc_lines {
-    my $self = shift;
-    my $opts;
-    if (ref($_[0]) eq 'HASH') { $opts = shift }
-    $opts //= {};
-
-    my @lines = map { $_ . (/\n\z/s ? "" : "\n") }
-        map {/\n/ ? split /\n/ : $_} @_;
-
-    my $indent = $self->indent x $self->indent_level;
-    push @{$self->doc_lines},
-        map {"$indent$_"} @lines;
-}
-
-sub add_function_section_before {
-    my ($self, $name, $before) = @_;
-    my $ss = $self->function_sections;
-    return unless $ss;
-    my $i = 0;
-    my $added;
-    while ($i < @$ss && defined($before)) {
-        if ($ss->[$i] eq $before) {
-            my $pos = $i;
-            splice @$ss, $pos, 0, $name;
-            $added++;
-            last;
-        }
-        $i++;
-    }
-    unshift @$ss, $name unless $added;
-}
-
-sub add_function_section_after {
-    my ($self, $name, $after) = @_;
-    my $ss = $self->function_sections;
-    return unless $ss;
-    my $i = 0;
-    my $added;
-    while ($i < @$ss && defined($after)) {
-        if ($ss->[$i] eq $after) {
-            my $pos = $i+1;
-            splice @$ss, $pos, 0, $name;
-            $added++;
-            last;
-        }
-        $i++;
-    }
-    push @$ss, $name unless $added;
-}
-
-sub delete_function_section {
-    my ($self, $name) = @_;
-    my $ss = $self->function_sections;
-    return unless $ss;
-    my $i = 0;
-    while ($i < @$ss) {
-        if ($ss->[$i] eq $name) {
-            splice @$ss, $i, 1;
-        } else {
-            $i++;
-        }
-    }
-}
-
-sub doc_parse_summary {
-    my ($self) = @_;
-
-    my ($name, $summary);
-
-    my $modname;
-    for ($modname) {
-        $_ = $self->{_info}{uri};
-        s!^pm:/!!;
-        s!/$!!;
-        s!/!::!g;
-    }
-
-    if ($self->{_meta}) {
-        $name    = $self->langprop($self->{_meta}, "name");
-        $summary = $self->langprop($self->{_meta}, "summary");
-    }
-    $name //= $modname;
-    $summary = "";
-
-    $self->doc_parse->{name}    = $name;
-    $self->doc_parse->{summary} = $summary;
-}
-
-sub doc_gen_summary {}
-
-sub doc_parse_version {
-    # already in meta's entity_version
-}
-
-sub doc_gen_version {}
-
-sub doc_parse_description {
-    my ($self) = @_;
-
-    $self->doc_parse->{description} = $self->{_meta} ?
-        $self->langprop($self->{_meta}, "description") : undef;
-}
-
-sub doc_gen_description {}
-
-sub fdoc_parse_summary {
-    my ($self) = @_;
-    my $p = $self->doc_parse->{functions}{ $self->{_furl} };
-
-    my $name = $self->langprop($self->{_fmeta}, "name");
-    if (!$name) {
-        $self->{_furl} =~ m!.+/(.+)!;
-        $name = $1;
-    }
-    my $summary = $self->langprop($self->{_fmeta}, "summary");
-    $p->{name}    = $name;
-    $p->{summary} = $summary;
-}
-
-sub fdoc_gen_summary {}
-
-sub fdoc_parse_description {}
-
-sub fdoc_gen_description {}
-
-sub fdoc_parse_links {}
-
-sub fdoc_gen_links {}
-
-sub fdoc_parse_arguments {
-    my ($self) = @_;
-    my $p      = $self->doc_parse->{functions}{ $self->{_furl} };
-    my $fmeta  = $self->{_fmeta};
-    my $fometa = $self->{_fometa};
-
-    my $aa = $fometa->{args_as} // $fmeta->{args_as} // 'hash';
-    my $paa;
-    if ($aa eq 'hash') {
-        $paa = '(%args)';
-    } elsif ($aa eq 'hashref') {
-        $paa = '(\%args)';
-    } elsif ($aa eq 'array') {
-        $paa = '(@args)';
-    } elsif ($aa eq 'arrayref') {
-        $paa = '(\@args)';
-    } else {
-        die "BUG: Unknown value of args_as '$aa'";
-    }
-    $p->{perl_args} = $paa;
-
-    my $args  = $fmeta->{args} // {};
-    $p->{args} = {};
-    for my $name (keys %$args) {
-        my $arg = $args->{$name};
-        $arg->{default_lang} //= $fmeta->{default_lang};
-        $arg->{schema} //= ['any'=>{}];
-        my $s = $arg->{schema};
-        my $pa = $p->{args}{$name} = {schema=>$s};
-        $pa->{human_arg} = Perinci::ToUtil::sah2human_short($s);
-        if (defined $s->[1]{default}) {
-            $pa->{human_arg_default} = dump1($s->[1]{default});
-        }
-        $pa->{summary}     = $self->langprop($arg, 'summary');
-        $pa->{description} = $self->langprop($arg, 'description');
-        $pa->{arg}         = $arg;
-        $pa->{req}         = $arg->{req}; # for convenience
-        #$pa->{pos}         = $arg->{pos}; # already available in 'arg'
-    }
-}
-
-sub fdoc_gen_arguments {}
-
-sub fdoc_parse_examples {
-}
-
-sub fdoc_gen_examples {}
-
-sub fdoc_parse_result {
-    my ($self) = @_;
-    my $p      = $self->doc_parse->{functions}{ $self->{_furl} };
-    my $fmeta  = $self->{_fmeta};
-    my $fometa = $self->{_fometa};
-
-    $p->{res_schema} = $fmeta->{result} ? $fmeta->{result}{schema} : undef;
-    $p->{res_schema} //= [any => {}];
-    $p->{human_res} = Perinci::ToUtil::sah2human_short($p->{res_schema});
-
-    my $rn = $fometa->{result_naked} // $fmeta->{result_naked};
-    if ($rn) {
-        $p->{human_ret} = $p->{human_res};
-    } else {
-        $p->{human_ret} = '[status, msg, result, meta]';
-    }
-
-    $p->{summary}     = $self->langprop($fmeta, "summary");
-    $p->{description} = $self->langprop($fmeta, "description");
-}
-
-sub fdoc_gen_result {}
-
-sub _fdoc_parse {
-    my ($self, $url) = @_;
-    $log->tracef("=> _fdoc_parse(url=%s)", $url);
-
-    my $fmeta;
-    my $fometa;
-    my $found;
-    {
-        if ($self->{_child_metas}) {
-            if ($fmeta = $self->{_child_metas}{$url}) {
-                $fometa = $self->{_child_orig_metas}{$url};
-                $found++;
-                last;
-            }
-        }
-
-        my $res = $self->_pa->request(meta => $url);
-        $res->[0] == 200 or die "Can't meta $self->{url}: ".
-            "$res->[0] - $res->[1]";
-        $fmeta  = $res->[2];
-        $fometa = $res->[3]{orig_meta};
-        $found++;
-        last;
-    }
-    die "BUG: Didn't find function metadata" unless $found;
-    $self->{_furl} = $url;
-    $self->{_fmeta} = $fmeta;
-    $self->{_fometa} = $fometa;
-
-    $self->doc_parse->{functions}{$url} = {meta=>$fmeta, orig_meta=>$fometa};
-    for my $s (@{ $self->function_sections // [] }) {
-        my $meth = "fdoc_parse_$s";
-        $log->tracef("=> $meth()");
-        $self->$meth;
-    }
-}
-
-sub _fdoc_gen {
-    my ($self, $url, %opts) = @_;
-    $log->tracef("=> _fdoc_gen(url=%s, opts=%s)", $url, \%opts);
-
-    my $p = $self->doc_parse->{functions}{$url};
-    for my $s (@{ $self->function_sections // [] }) {
-        my $meth = "fdoc_gen_$s";
-        $log->tracef("=> $meth()");
-        $self->$meth;
-    }
-}
-
-sub doc_parse_functions {
-    my ($self) = @_;
-
-    for my $e (@{ $self->{_children} }) {
-        next unless $e->{type} eq 'function';
-        $self->_fdoc_parse($e->{uri});
-    }
-}
-
-sub doc_gen_functions {
-    my ($self) = @_;
-
-    for my $e (@{ $self->{_children} }) {
-        next unless $e->{type} eq 'function';
-        $self->_fdoc_gen($e->{uri});
-    }
-}
-
-sub doc_parse_links {
-}
-
-sub doc_gen_links {}
-
-sub before_generate_doc {
+sub before_gen_doc {
     my ($self, %opts) = @_;
-    $log->tracef("=> PackageBase's before_generate_doc(opts=%s)", \%opts);
+    $log->tracef("=> PackageBase's before_gen_doc(opts=%s)", \%opts);
+
+    # initialize hash to store [intermediate] result
+    $self->{_res} = {};
 
     # let's retrieve the metadatas first
 
@@ -363,6 +83,92 @@ sub before_generate_doc {
     $self->{_child_orig_metas} = $res->[3]{orig_metas};
     #$log->tracef("child_metas=%s", $self->{_child_metas});
 
+}
+
+# provide simple default implementation without any text wrapping. subclass such
+# as Perinci::To::Text will use another implementation, one that supports text
+# wrapping for example (provided by
+# SHARYANTO::Role::Doc::Section::AddTextLines).
+sub add_doc_lines {
+    my $self = shift;
+    my $opts;
+    if (ref($_[0]) eq 'HASH') { $opts = shift }
+    $opts //= {};
+
+    my @lines = map { $_ . (/\n\z/s ? "" : "\n") }
+        map {/\n/ ? split /\n/ : $_} @_;
+
+    my $indent = $self->doc_indent x $self->doc_indent_level;
+    push @{$self->doc_lines},
+        map {"$indent$_"} @lines;
+}
+
+sub gen_doc_section_summary {
+    my ($self) = @_;
+
+    my ($name, $summary);
+
+    my $modname;
+    for ($modname) {
+        $_ = $self->{_info}{uri};
+        s!^pl:/!!;
+        s!/$!!;
+        s!/!::!g;
+    }
+
+    if ($self->{_meta}) {
+        $name    = $self->langprop($self->{_meta}, "name");
+        $summary = $self->langprop($self->{_meta}, "summary");
+    }
+    $name //= $modname;
+    $summary = "";
+
+    $self->{_res}{name}    = $name;
+    $self->{_res}{summary} = $summary;
+}
+
+sub gen_doc_section_version {
+}
+
+sub gen_doc_section_description {
+    my ($self) = @_;
+
+    $self->{_res}{description} = $self->{_meta} ?
+        $self->langprop($self->{_meta}, "description") : undef;
+}
+
+sub gen_doc_section_functions {
+    require Perinci::Sub::To::FuncBase;
+
+    my ($self) = @_;
+
+    # subclasses should override this method and provide the appropriate
+    # Perinci::Sub::To::* object in _fgen.
+    my $self->{_fgen} //= Perinci::Sub::To::FuncBase(
+        _pa => $self->_pa, # to avoid multiple instances of pa objects
+    );
+
+    # list all functions
+    my @func_uris = map { $_->{uri} }
+        grep { $_->{type} eq 'function' } @{ $self->{_children} // []};
+
+    # generate doc for all functions
+    my $fgen = $self->{_fgen};
+    $self->{_res}{functions} = {};
+    for my $furi (@func_uris) {
+        $fgen->url($furi);
+        if ($self->{_child_metas}) {
+            # avoid calling meta on furi again by fgen, since we have the meta
+            # already in _child_metas.
+            $fgen->{_meta}      = $self->{_child_metas}{$furi};
+            $fgen->{_orig_meta} = $self->{_child_orig_metas}{$furi};
+        }
+        $fgen->gen_doc();
+        $self->{_res}{functions}{$furi} = $fgen->doc_lines;
+    }
+}
+
+sub gen_doc_section_links {
 }
 
 1;
--- lib/Perinci/To/Text.pm
+++ lib/Perinci/To/Text.pm
@@ -13,130 +13,65 @@ sub BUILD {
     my ($self, $args) = @_;
 }
 
-sub doc_gen_summary {
+sub gen_doc_section_summary {
     my ($self) = @_;
+
+    $self->SUPER::gen_doc_section_summary;
 
     my $name_summary = join(
         "",
-        $self->doc_parse->{name} // "",
-        ($self->doc_parse->{name} && $self->doc_parse->{summary} ? ' - ' : ''),
-        $self->doc_parse->{summary} // ""
+        $self->{_res}{name} // "",
+        ($self->{_res}{name} && $self->{_res}{summary} ? ' - ' : ''),
+        $self->{_res}{summary} // ""
     );
 
     $self->add_doc_lines(uc($self->loc("Name")), "");
 
-    $self->inc_indent;
+    $self->inc_doc_indent;
     $self->add_doc_lines($name_summary);
-    $self->dec_indent;
+    $self->dec_doc_indent;
 }
 
-sub doc_gen_version {
+sub gen_doc_section_version {
     my ($self) = @_;
-
     $self->add_doc_lines("", uc($self->loc("Version")), "");
 
-    $self->inc_indent;
+    $self->inc_doc_indent;
     $self->add_doc_lines($self->{_meta}{entity_version} // '?');
-    $self->dec_indent;
+    $self->dec_doc_indent;
 }
 
-sub doc_gen_description {
+sub gen_doc_section_description {
     my ($self) = @_;
 
-    return unless $self->doc_parse->{description};
+    $self->SUPER::gen_doc_section_description;
+    return unless $self->{_res}{description};
 
     $self->add_doc_lines("", uc($self->loc("Description")), "");
 
-    $self->inc_indent;
-    $self->add_doc_lines($self->doc_parse->{description});
-    $self->dec_indent;
+    $self->inc_doc_indent;
+    $self->add_doc_lines($self->{_res}{description});
+    $self->dec_doc_indent;
 }
 
-sub _fdoc_gen {
-    my ($self, $url) = @_;
-    my $p = $self->doc_parse->{functions}{$url};
+sub gen_doc_section_functions {
+    require Perinci::Sub::To::Text;
 
-    $self->add_doc_lines(
-        "+ " . $p->{name} . $p->{perl_args} . ' -> ' . $p->{human_ret},
-    );
-    $self->inc_indent;
-
-    $self->add_doc_lines("", $p->{summary}) if $p->{summary};
-    $self->add_doc_lines("", $p->{description}) if $p->{description};
-    if (keys %{$p->{args}}) {
-        $self->add_doc_lines(
-            "",
-            $self->loc("Arguments") .
-                ' (' . $self->loc("'*' denotes required arguments") . '):',
-            "");
-        my $i = 0;
-        my $arg_has_ct;
-        for my $name (sort keys %{$p->{args}}) {
-            my $prev_arg_has_ct = $arg_has_ct;
-            $arg_has_ct = 0;
-            my $pa = $p->{args}{$name};
-            $self->add_doc_lines("") if $i++ > 0 && $prev_arg_has_ct;
-            $self->add_doc_lines(join(
-                "",
-                "- ", $name, ($pa->{req} ? '*' : ''), ' => ',
-                $pa->{human_arg},
-                (defined($pa->{human_arg_default}) ?
-                     " (" . $self->loc("default") .
-                         ": $pa->{human_arg_default})" : "")
-            ));
-            if ($pa->{summary} || $p->{description}) {
-                $arg_has_ct++;
-                $self->inc_indent;
-                $self->add_doc_lines("", $pa->{summary}) if $pa->{summary};
-                if ($pa->{description}) {
-                    $self->add_doc_lines("", $pa->{description});
-                }
-                $self->dec_indent;
-            }
-        }
-    }
-
-    if ($p->{meta}{dies_on_error}) {
-        $self->add_doc_lines("", $self->loc(
-            "This function dies on error."), "");
-    }
-
-    $self->add_doc_lines("", $self->loc("Return value") . ':', "");
-    $self->inc_indent;
-    my $rn = $p->{orig_meta}{result_naked} // $p->{meta}{result_naked};
-    $self->add_doc_lines($self->loc(join(
-        "",
-        "Returns an enveloped result (an array). ",
-        "First element (status) is an integer containing HTTP status code ",
-        "(200 means OK, 4xx caller error, 5xx function error). Second element ",
-        "(msg) is a string containing error message, or 'OK' if status is ",
-        "200. Third element (result) is optional, the actual result. Fourth ",
-        "element (meta) is called result metadata and is optional, a hash ",
-        "that contains extra information.")))
-        unless $rn;
-    $self->dec_indent;
-
-    $self->dec_indent;
-    $self->add_doc_lines("");
-
-    # test
-    #$self->add_doc_lines({wrap=>0}, "Line 1\nLine 2\n");
-}
-
-sub doc_gen_functions {
     my ($self) = @_;
-    my $pff = $self->doc_parse->{functions};
+
+    $self->{_fgen} //= Perinci::Sub::To::Text->new(
+        _pa => $self->_pa, # to avoid multiple instances of pa objects
+    );
 
     $self->add_doc_lines("", uc($self->loc("Functions")), "");
-    $self->inc_indent;
-
-    # XXX categorize functions based on tags
-    for my $url (sort keys %$pff) {
-        my $p = $pff->{$url};
-        $self->_fdoc_gen($url);
+    $self->inc_doc_indent;
+    $self->SUPER::gen_doc_section_functions;
+    for my $furi (sort keys %{ $self->{_res}{functions} }) {
+        my $fname;
+        for ($fname) { $_ = $furi; s!.+/!! }
+        $self->add_doc_lines($self->{_res}{functions}{$furi});
     }
-
-    $self->dec_indent;
+    $self->dec_doc_indent;
 }
 
 1;
@@ -150,17 +85,17 @@ sub doc_gen_functions {
 
  # to generate text documentation for the whole module
  my $doc = Perinci::To::Text->new(url => "/Some/Module/");
- say $doc->generate_doc;
+ say $doc->gen_doc;
 
-You can also try the L<peri-doc> script (included in the L<Perinci::To::POD>
+You can also try the L<peri-pkg-doc> script (included in the L<Perinci::To::POD>
 distribution) with the C<--format text> option:
 
- % peri-doc --format text Some::Module
+ % peri-pkg-doc --format text /Some/Module/
+
+To generate documentation for a single function, see L<Perinci::Sub::To::Text>
+or the provided command-line script L<peri-func-doc>.
 
 To generate a usage-like help message for a single function only, you can try
-L<Perinci::CmdLine>, using it like this:
-
- my $cmd = Perinci::CmdLine->new(url => "/Some/Module/func");
- $cmd->run_help; # will print help text
+the L<peri-func-usage> from the L<Perinci::CmdLine> distribution.
 
 =cut
